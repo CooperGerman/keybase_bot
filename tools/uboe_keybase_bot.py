7f54dbb5154ce1da33670a6b10bfc8ae3a519660
CooperGerman <yannickleprovost@wanadoo.fr>
2023-09-18 17:38:13 +0200

    [`SETUP`] : service config updated

--- tools/uboe_keybase_bot.py
+++ tools/uboe_keybase_bot.py
@@ -11,7 +11,7 @@
 import asyncio
 import logging as log
 import os
-import sys
+import sys, textwrap, re
 # import coloredlogs
 
 # import colored_traceback.auto
@@ -23,12 +23,39 @@ from pykeybasebot import Bot
 log.basicConfig(level=log.DEBUG)
 
 class Handler:
-    async def __call__(self, bot, event):
+    async def __call__(self, bot, event : chat1.Message):
         if event.msg.content.type_name != chat1.MessageTypeStrings.TEXT.value:
             return
-        if "🌴ping🌴" in event.msg.content.text.body :
-            channel = event.msg.channel
-            await bot.chat.send(channel, "🍹PONG!🍹")
+
+        # list all
+        if event.msg.sender == bot.username:
+            return
+
+        channel = event.msg.channel
+        if re.match(r'^/uboe_bot', event.msg.content.text.body):
+            # if "help" in event.msg.content.text.body :
+            if event.msg.content.text.body == "/uboe_bot help":
+                msg = textwrap.dedent("""
+                    Hello there! I'm uboe_bot, a bot for print farm management.
+                    I can help you with the following commands:
+                        `help` - this help message
+                        `status` - get the status of the print farm
+                    More commands coming soon!
+                """)
+            #if event.msg.content.text.body == "/uboe_bot status" :
+            elif event.msg.content.text.body == "/uboe_bot status" :
+                msg = textwrap.dedent(f"""
+                    {os.uname().nodename} is currently {os.getloadavg()[0]}% loaded.
+
+                """)
+
+            # if "🌴ping🌴" in event.msg.content.text.body :
+            elif "🌴ping🌴" in event.msg.content.text.body :
+                msg = "🍹PONG!🍹"
+            else :
+                msg = "Command not recognized. Try `/uboe_bot help`"
+
+            await bot.chat.send(channel, msg+bot.username)
 
 
 #open the paperkey file from path given in argument
