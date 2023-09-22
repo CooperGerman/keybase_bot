762ce87d79cf7d97dbbeb6e97795c342b855cd86
CooperGerman <yannickleprovost@wanadoo.fr>
2023-09-22 18:03:02 +0200

    [`LOG`] : info level for service + rollover added

--- scripts/run_keybase_bot.sh
+++ scripts/run_keybase_bot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 mkdir -p /home/$USER/keybase_bot/logs
 source /home/$USER/keybase_bot/.venv/bin/activate
-python /home/$USER/keybase_bot/tools/uboe_keybase_bot.py /home/$USER/.keybase_bot/paper_key --loglvl=debug
+python /home/$USER/keybase_bot/tools/uboe_keybase_bot.py /home/$USER/.keybase_bot/paper_key --loglvl=info
 deactivate
 
